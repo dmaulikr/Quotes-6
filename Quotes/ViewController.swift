43d110b83fb1ca49ad3ee905c621eb80dfc9903e
Bogdan98 <q8061@ya.ru>
2017-03-08 16:26:45 +0300

    First Commit

--- Quotes/ViewController.swift
+++ Quotes/ViewController.swift
@@ -9,15 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var buttonInspireMe: UIButton!
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var authorImage: UIImageView!
+    @IBOutlet weak var authorName: UILabel!
 
+    var game : QuotesGame = QuotesGame()
+    
+    
+    @IBAction func tapInspireMeButton(_ sender: Any) {
+        
+        if (game.currentQuoteIndex == game.quotes.count-1)
+        {
+            game.currentQuoteIndex = 0
+            startQuotes()
+        }
+        else{
+            updateQuotes()
+        }
+       
+    }
+    
+
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       super.viewDidLoad()
+        
+        startQuotes()
+        
     }
 
+    func updateQuotes(){
+        let nextQuote = game.getNextQuote()
+        let authorNameLet = game.getAuthorName()
+        let authorImageLet = UIImage(named: game.quotesAuthorImage[game.currentQuoteIndex])
+        textLabel.text = nextQuote
+        authorName.text = authorNameLet
+        authorImage.image = authorImageLet
+    }
+    
+    func startQuotes(){
+        let startedImage = UIImage(named: "thumb_steve")
+        authorImage.image = startedImage
+        textLabel.text = game.quotes[game.currentQuoteIndex]
+        authorName.text = game.quotesAuthor[game.currentQuoteIndex]
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
