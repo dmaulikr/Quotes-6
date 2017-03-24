f1efc91b48ec1c294aa586179212dbfbef8a2c8b
Bogdan98 <q8061@ya.ru>
2017-03-24 12:34:31 +0300

    v1.0

--- Quotes/ViewController.swift
+++ Quotes/ViewController.swift
@@ -1,65 +1,22 @@
-//
-//  ViewController.swift
-//  Quotes
-//
-//  Created by Богдан Быстрицкий on 02/12/2016.
-//  Copyright © 2016 Bogdan. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var quoteTextLabel: UILabel!
+    @IBOutlet weak var authorTextLabel: UILabel!
+    @IBOutlet weak var authorImageView: UIImageView!
     
-    @IBOutlet weak var buttonInspireMe: UIButton!
-    @IBOutlet weak var textLabel: UILabel!
-    @IBOutlet weak var authorImage: UIImageView!
-    @IBOutlet weak var authorName: UILabel!
+    var quoteBook = QuoteBook()
 
-    var game : QuotesGame = QuotesGame()
-    
-    
-    @IBAction func tapInspireMeButton(_ sender: Any) {
+    @IBAction func getNewQuoteTapped(_ sender: AnyObject)
+    {
+        let randomQuote = quoteBook.getRandomQuote()
+        let author = quoteBook.getAuthor(for: randomQuote)
+        let authorImageName = quoteBook.getImageName(for: author)
         
-        if (game.currentQuoteIndex == game.quotes.count-1)
-        {
-            game.currentQuoteIndex = 0
-            startQuotes()
-        }
-        else{
-            updateQuotes()
-        }
-       
+        quoteTextLabel.text = randomQuote
+        authorTextLabel.text = author
+        authorImageView.image = UIImage(named: authorImageName)
     }
-    
-
-    
-    override func viewDidLoad() {
-       super.viewDidLoad()
-        
-        startQuotes()
-        
-    }
-
-    func updateQuotes(){
-        let nextQuote = game.getNextQuote()
-        let authorNameLet = game.getAuthorName()
-        let authorImageLet = UIImage(named: game.quotesAuthorImage[game.currentQuoteIndex])
-        textLabel.text = nextQuote
-        authorName.text = authorNameLet
-        authorImage.image = authorImageLet
-    }
-    
-    func startQuotes(){
-        let startedImage = UIImage(named: "thumb_steve")
-        authorImage.image = startedImage
-        textLabel.text = game.quotes[game.currentQuoteIndex]
-        authorName.text = game.quotesAuthor[game.currentQuoteIndex]
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-
-
 }
 
